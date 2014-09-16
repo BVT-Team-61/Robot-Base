8cfaecd9862503b928966637313ab22bb148dfad
William Stevens <makefan123@yahoo.com>
2014-09-16 11:11:16 -0400

    Changed Drivetrain to use non-PID Subsystem
    Drivetrain extended the PIDSubsystem but never used any of its
    functionality. In the interest of simplicity it is now a plain
    subsystem.

--- src/edu/wpi/first/wpilibj/BasicRobot/subsystems/DriveTrain.java
+++ src/edu/wpi/first/wpilibj/BasicRobot/subsystems/DriveTrain.java
@@ -8,44 +8,27 @@ import edu.wpi.first.wpilibj.BasicRobot.RobotMap;
 import edu.wpi.first.wpilibj.BasicRobot.commands.DriveWithJoysticks;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Victor;
-import edu.wpi.first.wpilibj.command.PIDSubsystem;
+import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  * @author Team-61
  */
-public class DriveTrain extends PIDSubsystem {
+public class DriveTrain extends Subsystem {
     
 
-    private static final double Kp = 0.0;
-    private static final double Ki = 0.0;
-    private static final double Kd = 0.0;
-    
+
     private SpeedController leftMotor = new Victor(RobotMap.leftMotor);
     private SpeedController rightMotor = new Victor(RobotMap.rightMotor);
 
     // Initialize your subsystem here
     public DriveTrain() {
-        super("DriveTrain", Kp, Ki, Kd);
-
-        
+        super("DriveTrain");  
     }
     
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         setDefaultCommand(new DriveWithJoysticks());
-    }
-    
-    protected double returnPIDInput() {
-        // Return your input value for the PID loop
-        // e.g. a sensor, like a potentiometer:
-        // yourPot.getAverageVoltage() / kYourMaxVoltage;
-        return 0.0;
-    }
-    
-    protected void usePIDOutput(double output) {
-        // Use output to drive your system, like a motor
-        // e.g. yourMotor.set(output);
     }
     
     /**
